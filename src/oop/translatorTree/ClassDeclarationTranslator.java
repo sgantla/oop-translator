d0469d04986dac1d34870b3fe070b327977c4101
Maxwell Mosley <maxwell.mosley@gmail.com>
2012-10-16 23:18:17 -0400

    Added ClassDeclarationTranslator.java.

---
+++ src/oop/translatorTree/ClassDeclarationTranslator.java
@@ -0,0 +1,73 @@
+package oop.translatorTree;
+
+import java.util.List;
+import java.util.ArrayList;
+import xtc.tree.Node;
+
+class ClassDeclarationTranslator extends DeclarationTranslator 
+    implements ClassDeclaration {
+
+    private class Input { 
+        List<String> modifiers = new ArrayList<String>();
+        String className;
+        String extension;
+    }
+    private class Output {
+        List<Modifier> modifiers = new ArrayList<Modifier>();
+        List<String> extensions = new ArrayList<String>(); // for his exceptions
+        String className;
+        ClassBodyTranslator classBody;
+    }
+
+    private Input java = new Input();
+    private Output cpp = new Output();
+
+    public List<Modifier> getModifiers() {
+        return cpp.modifiers;
+    }
+
+    public String getClassName() {
+        return cpp.className;
+    }
+
+    public List<String> getExtensions() {
+        return cpp.extensions;
+    }
+
+    public ClassBody getClassBody() {
+        return cpp.classBody;
+    }
+
+    public ClassDeclarationTranslator(TranslatorNode parent) {
+        super(parent);
+    }
+    
+    public CppAstUtil.NodeName getNodeType () {
+        return CppAstUtil.NodeName.ClassDeclaration;
+    }
+
+    public void initialize(Node n) {
+        Node modifiersNode = JavaAstUtil.getChildByName(n, JavaAstUtil.NodeName.Modifiers);
+        if (modifiersNode != null) {
+            for (Node modifierNode : JavaAstUtil.getChildrenByName(modifiersNode, JavaAstUtil.NodeName.Modifier)) {
+                java.modifiers.add(JavaAstUtil.extractString(modifierNode)); // Not used
+            }
+        }
+
+        Node extensionNode = JavaAstUtil.getChildByName(n, JavaAstUtil.NodeName.Extension);
+        if (extensionNode != null) {
+            java.extension = JavaAstUtil.extractString(extensionNode);
+        }
+    
+        java.className = n.getString(1);
+        cpp.className = java.className;
+        cpp.modifiers.add(Modifier.PUBLIC); // we don't need to worry about package-private classes
+                                            // just make all classes public
+    
+        Node classBodyNode = JavaAstUtil.getChildByName(n, JavaAstUtil.NodeName.ClassBody);
+        if (classBodyNode != null) {
+            cpp.classBody = new ClassBodyTranslator(this);
+            cpp.classBody.initialize(classBodyNode);
+        }
+    }
+}
