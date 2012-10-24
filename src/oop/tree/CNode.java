8feee4b966bfe172c3cbdaa8f32d32bb9e156c4d
Harsh Sinha <indianbrothahs@gmail.com>
2012-10-24 19:43:34 -0400

    Added CppPrinter Switch

--- src/oop/tree/CNode.java
+++ src/oop/tree/CNode.java
@@ -1,6 +1,12 @@
 package oop.tree;
+import oop.translator.CppAstUtil;
 
 public abstract class CNode {
 
     CNode _parent;
+    CppAstUtil.NodeName name;
+    
+    public CppAstUtil.NodeName getName() {
+      return this.name;
+    }
 }
