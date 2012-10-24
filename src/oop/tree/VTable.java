6d295a061016cf58358786f77d3331c56ebfc6fb
Jen Mak <jmm826@nyu.edu>
2012-10-24 19:43:32 -0400

    changed import statement

--- src/oop/tree/VTable.java
+++ src/oop/tree/VTable.java
@@ -3,7 +3,7 @@ package oop.tree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VTable extends Node {
+public class VTable extends CNode {
     List<FieldDeclaration> vTableMembers;
 
     public VTable clone() {
