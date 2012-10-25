9a4c9c16d4e64d2be47d16cba3b41deaaeaee037
Jen Mak <jmm826@nyu.edu>
2012-10-25 02:03:58 -0400

    NameSpaceDeclaration changed

--- src/oop/tree/NamespaceDeclaration.java
+++ src/oop/tree/NamespaceDeclaration.java
@@ -1,7 +1,9 @@
 package oop.tree;
 
 public class NamespaceDeclaration extends Declaration {
+	
+	// Symbols declared inside a namespace block are placed in a named scope
+	public String name;
+	public List<Declaration> declarations;
 
-    public NamespaceDeclaration() {
-    }
 }
